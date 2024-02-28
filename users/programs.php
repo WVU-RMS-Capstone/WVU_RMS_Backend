651d213fd14178aff6f0b6df53f5b3f78d66723a
Michael Hogue <michael.hogue.19@gmail.com>
2024-02-28 14:04:17 -0500

    Fix JSON response in createExercise

--- users/programs.php
+++ users/programs.php
@@ -10,17 +10,31 @@
         
         if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['action'] === 'createexercise') {
             createExercise();
-        }
-        else{
+        } else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['action'] === 'fetchpremadeprograms')
+        else {
             echo "Specified action not available.";
             http_response_code(201);
             exit();
         }
     }
+    
     /*
     Description: 
 
-    Return: True stating the workout was added
+    Return: Set of all premade programs
+
+    Example: https://restapi-playerscompanion.azurewebsites.net/users/auth.php?action=fetchpremadeprograms
+    */
+    function fetchPremadePrograms() {
+        $database = new database();
+        $db = $database->getConnection();
+    }
+    
+    /*
+    Description: 
+
+    Return: True stating the exercise was added (200), False if something went wrong (500), or the ID of a 
+    duplicate exercise which already exists (409) 
 
     Example: https://restapi-playerscompanion.azurewebsites.net/users/auth.php?action=createxercise&Video=https://words.com&Cover=img.img&Name=rdl&Description=one-legged-deadlifts&Sets=3&Reps=10&BodyPart=knee
     */
@@ -41,10 +55,9 @@
         $check = "SELECT exerciseID FROM [dbo].[Exercises] WHERE Name = '$name'";
         $res = sqlsrv_query($db, $check);
         $r = sqlsrv_fetch_array( $res, SQLSRV_FETCH_NUMERIC );
-        echo 'reaching';
         if( $r !== NULL ){
-            echo 'Exercise Already Exists.';
-            echo json_encode("ID: $r[0]");
+            // echo 'Exercise Already Exists.';
+            echo json_encode($r[0]);
             http_response_code(409); 
             sqlsrv_free_stmt($res);
             sqlsrv_close($db);
@@ -54,9 +67,10 @@
         $sql = "INSERT INTO [dbo].[Exercises] (Video, Cover, Name, Description, Sets, Reps, BodyPart) VALUES ('$video', '$cover', '$name', '$description', '$sets', '$reps', '$body_part')";
         $stmt = sqlsrv_query($db, $sql);
         if($stmt === False){  
-            echo "Error in statement preparation/execution.\n";  
+            // echo "Error in statement preparation/execution.\n";  
             exit( print_r( sqlsrv_errors(), True));  
             echo json_encode(False);
+            http_response_code(500);
             return False;
         }
         echo json_encode(True);
