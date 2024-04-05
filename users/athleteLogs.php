db78549004f0ace66330453945a3dc87d5bc1c48
Tyler Gilhuys <tg0046@mix.wvu.edu>
2024-04-05 11:53:00 -0400

    update get notes

--- users/athleteLogs.php
+++ users/athleteLogs.php
@@ -142,7 +142,7 @@
 
         $athlete = $_GET['Athlete'];
 
-        $sql = "SELECT Date, Note, MadeBy FROM [dbo].[Notes] WHERE Athlete = '$athlete'";
+        $sql = "SELECT Date, Note, MadeBy FROM [dbo].[Notes] WHERE Athlete = '$athlete' ORDER BY Date ASC";
         $stmt = sqlsrv_query($db, $sql);
         if ($stmt === false) {
             echo "Something went wrong fetching the notes";
