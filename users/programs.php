a735f1ba671fb9b4b593dd0fa6800a084629241f
Michael Hogue <michael.hogue.19@gmail.com>
2024-04-15 15:12:53 -0400

    Just get workout count as URL parameter

--- users/programs.php
+++ users/programs.php
@@ -276,14 +276,9 @@
         $workout_8 = $_GET['Workout8'];
         $workout_9 = $_GET['Workout9'];
         $workout_10 = $_GET['Workout10'];
-        $i = 0;
-        
-        for ($i = 1; $i <= 10; $i++) {
-            echo strcmp($_GET["Workout$i"], '0');
-            if (!strcmp($_GET["Workout$i"], '0')) break;
-        }
+        $workout_count = $_GET['WorkoutCount'];
 
-        $sql = "INSERT INTO [dbo].[Program_Exercises] (Workout_1, Workout_2, Workout_3, Workout_4, Workout_5, Workout_6, Workout_7, Workout_8, Workout_9, Workout_10, Workout_Count) VALUES ('$workout_1', '$workout_2', '$workout_3', '$workout_4', '$workout_5', '$workout_6', '$workout_7', '$workout_8', '$workout_9', '$workout_10', '$i')";
+        $sql = "INSERT INTO [dbo].[Program_Exercises] (Workout_1, Workout_2, Workout_3, Workout_4, Workout_5, Workout_6, Workout_7, Workout_8, Workout_9, Workout_10, Workout_Count) VALUES ('$workout_1', '$workout_2', '$workout_3', '$workout_4', '$workout_5', '$workout_6', '$workout_7', '$workout_8', '$workout_9', '$workout_10', '$workout_count')";
         $stmt = sqlsrv_query($db, $sql);
         if ($stmt === False) {
             // echo "Error in statement preparation/execution.\n";  
